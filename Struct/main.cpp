24a85a75d5e6116c087c76ed88f6a10a7d0097e0
Zaher Natour <zaher@zaher-macbook-pro.local>
2018-12-04 14:23:11 +0800

    Initial Commit

---
+++ Struct/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Struct
+//
+//  Created by Zaher Natour on 04/12/2018.
+//  Copyright © 2018 Zaher Natour. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
